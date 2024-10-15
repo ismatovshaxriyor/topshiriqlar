6fcec9c5c32324d45dfb245ffb1b0319f0fe078d
shaxa-dev-000000 <shaxriyorismatov14@gmail.com>
2024-10-15 19:58:09 +0500

    Update topshiriq4.py

--- topshiriq4.py
+++ topshiriq4.py
@@ -1,4 +1,6 @@
 a = input("4 xonali son kiriting: ")
-kopaytma = int(a[0]) * int(a[1]) * int(a[2]) * int(a[3])
-
-print(f'Raqamlar ko\'paytmasi: {kopaytma}')
+if len(a) == 4:
+    kopaytma = int(a[0]) * int(a[1]) * int(a[2]) * int(a[3])
+    print(f'Raqamlar ko\'paytmasi: {kopaytma}')
+else:
+    print("Siz 4 xonali son kiritmadingiz!")
