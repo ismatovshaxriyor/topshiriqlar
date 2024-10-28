dad59fdbee2a2edc4bb7bef2a1870f53df562ecd
shaxa-dev-000000 <shaxriyorismatov14@gmail.com>
2024-10-28 12:17:38 +0500

    Update topshiriq3.py

--- topshiriq3.py
+++ topshiriq3.py
@@ -1,20 +1,24 @@
-x = int(input('x sonni kiriting: '))
+x = float(input('x sonni kiriting: '))
 i = 8
 c = 0
 
-while i != 0:
-    a = x**2
-    print(f"a = {a}")
-    if i == 8:
-        b = (2**i)/(x**2)
-        print(f'b = {b}')
-    else:
-        b = 2**i/c
-        print(f'b = {b}')
-    i -= 1
-    c = a + b
-    print(f'c = {c}')
+if x != 0:
+    while i != 0:
+        a = x**2
+        print(f"a = {a}")
+        if i == 8:
+            b = (2**i)/(x**2)
+            print(f'b = {b}')
+        else:
+            b = 2**i/c
+            print(f'b = {b}')
+        i -= 1
+        c = a + b
+        print(f'c = {c}')
 
-javob = x / c
-print("Javobi:", javob)
+    javob = x / c
+    print("Javobi:", javob)
+
+else:
+    print("x soni 0 ga teng bo'lishi mumkin emas")
 
