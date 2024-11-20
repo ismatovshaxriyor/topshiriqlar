fe6998bc7150066904f73b1687006870afd1078c
shaxa-dev-000000 <shaxriyorismatov14@gmail.com>
2024-11-20 16:16:29 +0500

    Update topshiriq7.py

--- topshiriq7.py
+++ topshiriq7.py
@@ -14,39 +14,40 @@ kichik_sonlar = []
 kichik_sonlar_index = []
 
 def for_max():
-    for row in matritsa:
-        max_element = max(row)
-        max_element_index = row.index(max_element)
-        katta_sonlar.append(max_element)
-        katta_sonlar_index.append(max_element_index)
+    for row in matritsa: # matritsadan har bir qatorni ajratib oladi
+        max_element = max(row) # har bir qatordagi maksimal qiymatni oladi
+        max_element_index = row.index(max_element) # va o'sha maksilmal qiymatning indeksini aniqlaydi
+        katta_sonlar.append(max_element) # olingan katta sonni katta_sonlar listiga qo'shadi
+        katta_sonlar_index.append(max_element_index) # va o'sha katta sonning olingan indeksini katta_sonlar_index listiga qo'shadi
     return katta_sonlar, katta_sonlar_index
 
 def for_min():
-    for row in matritsa:
-        min_element = min(row)
-        min_element_index = row.index(min_element)
-        kichik_sonlar.append(min_element)
-        kichik_sonlar_index.append(min_element_index)
+    for row in matritsa: # matritsadan har bir qatorni ajratib oladi
+        min_element = min(row) # har bir qatordagi minimal qiymatni oladi
+        min_element_index = row.index(min_element) # va o'sha minimal qiymatning indeksini aniqlaydi
+        kichik_sonlar.append(min_element) # olingan kichik sonni kichik_sonlar listiga qo'shadi
+        kichik_sonlar_index.append(min_element_index) # va o'sha kichik sonning olingan indeksini kichik_sonlar_index listiga qo'shadi
     return kichik_sonlar, kichik_sonlar_index
 
-maximums, max_indexs = for_max()
-minimums, min_indexs = for_min()
+maximums, max_indexs = for_max() # har bir qatordagi katta sonlarni maximums o'zgaruvchisiga, ularning indekslarini max_indexs o'zgaruvchisiga qo'shib oladi
+minimums, min_indexs = for_min() # har bir qatordagi kichik sonlarni minimums o'zgaruvchisiga, ularning indekslarini min_indexs o'zgaruvchisiga qo'shib oladi
 
-matritsa_max = max(maximums)
+matritsa_max = max(maximums) # butun matritsadagi eng katta qiymatni oladi
 print(f'matritsaning maksimal qiymati: {matritsa_max}')
-matritsa_min = min(minimums)
+matritsa_min = min(minimums) # butun matritsadagi eng kichik qiymatni oladi
 print(f'matritsaning minimal qiymati: {matritsa_min}')
 
-matritsa_max_index1 = maximums.index(matritsa_max)
-matritsa_max_index2 = max_indexs[matritsa_max_index1]
+matritsa_max_index1 = maximums.index(matritsa_max) # matritsaning eng katta qiymatining qator bo'yicha indeksini oladi
+matritsa_max_index2 = max_indexs[matritsa_max_index1] # matritsaning eng katta qiymatining ustun bo'yicha indeksini oladi
 print(f"matritsaning maksimal qiymati {matritsa_max_index1 + 1}-qatorning {matritsa_max_index2 + 1}-tartibida joylashgan")
 
-matritsa_min_index1 = minimums.index(matritsa_min)
-matritsa_min_index2 = min_indexs[matritsa_min_index1]
+matritsa_min_index1 = minimums.index(matritsa_min) # matritsaning eng kichik qiymatining qator bo'yicha indeksini oladi
+matritsa_min_index2 = min_indexs[matritsa_min_index1] # matritsaning eng kichik qiymatining ustun bo'yicha indeksini oladi
 print(f"matritsaning minimal qiymati {matritsa_min_index1 + 1}-qatorning {matritsa_min_index2 + 1}-tartibida joylashgan")
 
 matritsa[matritsa_max_index1][matritsa_max_index2], matritsa[matritsa_min_index1][matritsa_min_index2] = matritsa[matritsa_min_index1][matritsa_min_index2], matritsa[matritsa_max_index1][matritsa_max_index2]
+# ularning o'rni almashtiriladi
 
 for row in matritsa:
-    print(row)
+    print(row) # matritsaning har bir qatorini chiqarib beradi
 
