ab929fbc8a1260d27e0264f2eaaf6cac8e8a1a74
shaxa-dev-000000 <shaxriyorismatov14@gmail.com>
2024-12-02 16:21:00 +0500

    Update topshiriq5.py

--- topshiriq5.py
+++ topshiriq5.py
@@ -1,37 +1,40 @@
-i = input("koordinatalar sonini kiriting: ")
-n = int(i)
+n = int(input("fazoda nechta nuqta bor: "))
 
-def x():
-    i = 1
-    xN = 0
-    while i < (n + 1):
-        xN += i
+x_row = []
+y_row = []
+z_row = []
+
+def koordinatalar():
+    for i in range(n):
+        x_koordinata = float(input(f"x{i+1}-koordinatani kiriting: "))
+        x_row.append(x_koordinata)
+        y_koordinata = float(input(f"y{i+1}-koordinatani kiriting: "))
+        y_row.append(y_koordinata)
+        z_koordinata = float(input(f"z{i+1}-koordinatani kiriting: "))
+        z_row.append(z_koordinata)
+    return x_row, y_row, z_row
+
+x_koordinatalar, y_koordinatalar, z_koordinatalar = koordinatalar()
+
+def markaz():
+    i = 0
+    x_markaz = 0
+    y_markaz = 0
+    z_markaz = 0
+    while i < n:
+        x_markaz = x_markaz + x_koordinatalar[i]
+        y_markaz = y_markaz + y_koordinatalar[i]
+        z_markaz = z_markaz + z_koordinatalar[i]
         i += 1
-    return float(xN/n)
+    return x_markaz/n, y_markaz/n, z_markaz/n
 
-def y():
-    i = 1
-    yN = 0
-    while i < (n + 1):
-        yN += i
-        i += 1
-    return float(yN/n)
-
-def z():
-    i = 1
-    zN = 0
-    while i < (n + 1):
-        zN += i
-        i += 1
-    return float(zN/n)
-
-def masofalar():
-    i = 1
-    while i < (n + 1):
-        d = ((i - x())**2 + (i - y())**2 + (i - z())**2)**(1/2)
-        print(f"massa markazidan {i}-nuqtagacha bo'lgan masofa: {d}")
+def markazdan_nuqtagacha():
+    i = 0
+    while i < n:
+        masofa = ((x_row[i] - markaz()[0])**2 + (y_row[i] - markaz()[1])**2 + z_row[i] - markaz()[2]**2)**0.5
+        print(f"Markazdan {i+1}-nuqta gacha masofa: {masofa}")
         i += 1
 
+print(f'Markaz koordinatalari: {markaz()}')
+markazdan_nuqtagacha()
 
-print(f"massa markazi ({x()}, {y()}, {y()}) nuqtada")
-masofalar()
