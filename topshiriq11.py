d8d11c5ec43ad3238f59e6e32bc0ef65e2055ee9
shaxa-dev-000000 <shaxriyorismatov14@gmail.com>
2025-02-07 16:47:57 +0500

    Update topshiriq11.py

--- topshiriq11.py
+++ topshiriq11.py
@@ -32,4 +32,44 @@
 #         break
 
 
-""" ---------------------- 26 - masala ----------------------- """
+# """ ---------------------- 26 - masala ----------------------- """
+# word = input("So'z kiriting: ")
+# secret_word = ''
+# unsecret_word = ''
+
+# while True:
+#     shart = input("1. So'zni shifrlash \n2. So'zni deshifrlash \n3. Chiqish \n-> ")
+#     if shart == "1":
+#         qism1 = ''
+#         qism2 = ''
+
+#         for i in range(len(word)):
+#             if i % 2 == 1:
+#                 qism1 += word[i]
+#             else:
+#                 qism2 += word[i]
+#         secret_word = qism1 + qism2[::-1]
+#         print(f"Shifrlangan so'z: {secret_word}\n")
+#     elif shart == '2':
+#         if len(secret_word) == 0:
+#             print("So'zni deshifrlashdan oldin uni shifrlangan")
+#         else:
+#             qism1 = list(secret_word[:(len(secret_word)//2)])
+#             qism2 = list(secret_word[-len(secret_word)//2:][::-1])
+
+#             for i in range(len(secret_word)):
+#                 if i % 2 != 1:
+#                     unsecret_word += qism2[0]
+#                     del qism2[0]
+#                 elif i % 2 != 1 and len(qism1) == 0:
+#                     unsecret_word += qism2[0]
+#                     del qism2[0]
+#                 else:
+#                     unsecret_word += qism1[0]
+#                     del qism1[0]
+#             print(f"Deshifrlangan so'z: {unsecret_word}\n")
+#             unsecret_word = ''
+#     elif shart == "3":
+#         break
+#     else:
+#         print("Noto'g'ri buyruq kiritildi.\n")
