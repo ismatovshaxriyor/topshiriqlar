222493d882424543a7894e8177de591e07c0895e
shaxa-dev-000000 <shaxriyorismatov14@gmail.com>
2025-03-04 18:25:16 +0500

    .

--- dasturlash/topshiriq11.py
+++ dasturlash/topshiriq11.py
@@ -1,37 +1,37 @@
 """ ---------------------- 7 - masala ----------------------- """
-# txt = "Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took galley of type and scrambled it to make type specimen book."
-txt = "I will be by far the most brilliant programmer in the world!!!."
-
-txt_list = txt.split()
-new_txt_list = []
-
-while txt:
-    shart = input("-> ")
-
-    if shart == "a":
-        for word in txt_list:
-            if "." not in word:
-                new_word = word[1:] + word[0]
-                new_txt_list.append(new_word)
-        print(new_txt_list)
-        new_txt_list.clear()
-    elif shart == "b":
-        for word in txt_list:
-            if "." not in word:
-                new_word = word[-1] + word[:-1]
-                new_txt_list.append(new_word)
-        print(new_txt_list)
-        new_txt_list.clear()
-    elif shart == "c":
-        for word in txt_list:
-            if "." not in word:
-                new_word = word[1:]
-                new_txt_list.append(new_word)
-        print(new_txt_list)
-        new_txt_list.clear()
-    else:
-        print("Command not found :(")
-        break
+# # txt = "Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took galley of type and scrambled it to make type specimen book."
+# txt = "I will be by far the most brilliant programmer in the world!!!."
+#
+# txt_list = txt.split()
+# new_txt_list = []
+#
+# while txt:
+#     shart = input("-> ")
+#
+#     if shart == "a":
+#         for word in txt_list:
+#             if "." not in word:
+#                 new_word = word[1:] + word[0]
+#                 new_txt_list.append(new_word)
+#         print(new_txt_list)
+#         new_txt_list.clear()
+#     elif shart == "b":
+#         for word in txt_list:
+#             if "." not in word:
+#                 new_word = word[-1] + word[:-1]
+#                 new_txt_list.append(new_word)
+#         print(new_txt_list)
+#         new_txt_list.clear()
+#     elif shart == "c":
+#         for word in txt_list:
+#             if "." not in word:
+#                 new_word = word[1:]
+#                 new_txt_list.append(new_word)
+#         print(new_txt_list)
+#         new_txt_list.clear()
+#     else:
+#         print("Command not found :(")
+#         break
 
 
 """ ---------------------- 26 - masala ----------------------- """
