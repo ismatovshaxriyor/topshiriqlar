a141ad2d93d04db64dc59c465ce2421ed8fb852f
shaxa-dev-000000 <shaxriyorismatov14@gmail.com>
2025-04-19 12:48:57 +0500

    .

--- algoritmlar/topshiriq4.py
+++ algoritmlar/topshiriq4.py
@@ -30,9 +30,84 @@ class BST:
             else:
                 break
 
-    def inorder(self):
+    # <=== 1 - topshiriq ===>
+    def Count_toq(self):
+        count = 0
         tugun = self.root
-        while tugun:
+        while tugun is not None:
+            if tugun.Left is None:
+                if tugun.Data % 2 == 1:
+                    count += 1
+                tugun = tugun.Right
+            else:
+                old_tugun = tugun.Left
+                while old_tugun.Right and old_tugun.Right != tugun:
+                    old_tugun = old_tugun.Right
+
+                if old_tugun.Right is None:
+                    old_tugun.Right = tugun
+                    tugun = tugun.Left
+                else:
+                    if tugun.Data % 2 == 1:
+                        count += 1
+                    old_tugun.Right = None
+                    tugun = tugun.Right
+        print("Toq sonlar soni:", count)
+
+    # <=== 2 - topshiriq ===>
+    def search(self, num):
+        topildi = False
+        tugun = self.root
+        while tugun is not None:
+            if tugun.Left is None:
+                if tugun.Data == num:
+                    topildi =True
+                tugun = tugun.Right
+            else:
+                old_tugun = tugun.Left
+                while old_tugun.Right and old_tugun.Right != tugun:
+                    old_tugun = old_tugun.Right
+
+                if old_tugun.Right is None:
+                    old_tugun.Right = tugun
+                    tugun = tugun.Left
+                else:
+                    if tugun.Data == num:
+                        topildi =True
+                    old_tugun.Right = None
+                    tugun = tugun.Right
+        
+        if topildi:
+            print(f"{num} soni daraxtda mavjud")
+        else:
+            print(f"{num} soni daraxtda mavjud emas")
+
+    # <=== 3 - topshiriq ===>
+    def chech_brother(self):
+        def tekshir(tugun):
+            if tugun is None:
+                return
+            if tugun.Left and tugun.Right:
+                print(f"{tugun.Left.Data} va {tugun.Right.Data} aka uka")
+            tekshir(tugun.Left)
+            tekshir(tugun.Right)
+
+        tekshir(self.root)
+
+    # <=== 4 - topshiriq ===>
+    def go_right(self):
+        if self.root is None:
+            return None
+
+        tugun = self.root
+        while tugun.Right is not None:
+            tugun = tugun.Right
+
+        print(f"eng uchidagi son: {tugun.Data}")
+
+    def print(self):
+        tugun = self.root
+        while tugun is not None:
             if tugun.Left is None:
                 print(tugun.Data)
                 tugun = tugun.Right
@@ -49,15 +124,21 @@ class BST:
                     old_tugun.Right = None
                     tugun = tugun.Right
 
-
 daraxt = BST()
 
 daraxt.add(5)
 daraxt.add(3)
+daraxt.add(10)
 daraxt.add(7)
 daraxt.add(2)
-daraxt.add(4)
+daraxt.add(9)
 daraxt.add(6)
 daraxt.add(8)
 
-daraxt.inorder()  
+daraxt.search(3)
+daraxt.chech_brother()
+daraxt.go_right()
+
+daraxt.print()
+
+daraxt.Count_toq()
--- dasturlash/topshiriq13.py
+++ dasturlash/topshiriq13.py
@@ -36,6 +36,31 @@ class Navbat:
             print(cur.Data)
             cur = cur.Next
 
+class MUSBAT_MANFIY_NAVBATLAR(Navbat):
+    def __init__(self):
+        super().__init__()
+
+    def saralash(self, lst):
+        self.musbat_navbat = Navbat()
+        self.manfiy_navbat = Navbat()
+        for i in lst:
+            if i > 0:
+                self.musbat_navbat.Navbatga(i)
+            else:
+                self.manfiy_navbat.Navbatga(i)
+
+    def Print_musbat(self):
+        cur = self.musbat_navbat.boshi
+        while cur is not None:
+            print(cur.Data)
+            cur = cur.Next
+
+    def Print_manfiy(self):
+        cur = self.manfiy_navbat.boshi
+        while cur is not None:
+            print(cur.Data)
+            cur = cur.Next
+
 navbat = Navbat()
 
 navbat.Navbatga(1)
@@ -52,4 +77,9 @@ navbat.Navbatga(3)
 
 # navbat.Navbatdan()
 
-navbat.Print()
+lst = [1, -2, 3, -4, 5, -6]
+MNN = MUSBAT_MANFIY_NAVBATLAR()
+MNN.saralash(lst)
+
+MNN.Print_musbat()
+MNN.Print_manfiy()
